82b72a187d448f5d01caddfcaf962ee60a70ee8c
ANANTH4004 <balamuruganananth@gmail.com>
2022-11-08 22:46:21 +0530

    WebAPI

---
+++ API/VaccineManagementAPI/Models/Center.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VaccineManagementAPI.Models
+{
+    public class Center
+    {
+        [Key]
+        public int CenterId { get; set; }
+        [Required]
+        public string CenterAddress { get; set; }
+        public int Pin { get; set; }
+        public string City { get; set; }
+       // public virtual ICollection<VaccinationStock> VaccinationStocks { get; set; }
+      //  public virtual ICollection<Slot> Slots { get; set; }
+
+
+    }
+}
